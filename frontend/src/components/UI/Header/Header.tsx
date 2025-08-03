f8dd26430ac1972dd5951ed5b310435b9b80661f
IvanovSE <postironiy2003@gmail.com>
2025-08-03 10:38:24 +0500

    7-front commit

--- frontend/src/components/UI/Header/Header.tsx
+++ frontend/src/components/UI/Header/Header.tsx
@@ -1,10 +1,17 @@
 import './Header.scss'
 import logo from '../../../assets/images/logo.png'
 import avatar from '../../../assets/images/avatar.png'
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router'
+import { Context } from '../../../main'
 
-const Header: React.FC =() => {
+const Header: React.FC = () => {
+    const { store } = useContext(Context)
+
+    const logout = () => {
+        store.logout();
+    }
+
     return (
         <>
             <div className='header'>
@@ -19,9 +26,13 @@ const Header: React.FC =() => {
                     <a href="#">Цены</a>
                 </nav>
                 <div className='header__profile'>
-                    <div className='profile__number'>
-                        номер телефона на главной панели
-                    </div>
+                    {store.isAuth ? (
+                        <button onClick={logout}>Выйти</button>
+                    ) : (
+                        <Link to="/register">
+                            <button>Войти</button>
+                        </Link>
+                    )}
                     <Link to="/register">
                         <img src={avatar} alt="avatar" />
                     </Link>
